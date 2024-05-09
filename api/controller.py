f79359ca5f03b5351f7349ef08bb96f906ffe18f
sonesh.s <sonesh.s@MMMDVSONESHS.local>
2024-05-09 12:52:09 +0530

    new changes 27

--- api/controller.py
+++ api/controller.py
@@ -9,14 +9,15 @@ import ta
 def getRSI():
     flock_url = 'https://api.flock.com/hooks/sendMessage/524b8519-9a8f-4ef0-85e6-67321ae7adf9'
     symbol_list = ['NSE_INDEX|Nifty 50','NSE_INDEX|Nifty Bank','NSE_EQ|INE002A01018']
+    symbol_name = ['Nifty 50','Nifty Bank','Reliance']
     payload = {
         "text": "Relative Strength Index",
         "attachments": []
     }
-    for sym in symbol_list:
-        payload['attachments'].append({
+    for sym,sym_name in zip(symbol_list,symbol_name):
+        payload['attachments']=[{
                 "title": "From Controller",
-                "description": sym,
+                "description": sym_name,
                 "views": {
                     "image": {
                         "original": {
@@ -31,10 +32,9 @@ def getRSI():
                         }
                     }
                 }
-            })
-
-    print(payload)
-    requests.post(flock_url,json=payload)
+            }]
+        print(payload)
+        requests.post(flock_url,json=payload)
     return
 
 
